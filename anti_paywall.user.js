3d000f58e6733175eebd3cddc23a8762bf550f6b
Abdussamet Kocak <abdus@abdus.dev>
2025-08-14 15:46:16 +0200

    fix: Fix update URLs

--- anti_paywall.user.js
+++ anti_paywall.user.js
@@ -6,8 +6,8 @@
 // @author       https://github.com/abdusco
 // @match        *://*/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=nytimes.com
-// @updateURL    https://raw.githubusercontent.com/abdusco/userscripts/master/anti_paywall.js
-// @downloadURL  https://raw.githubusercontent.com/abdusco/userscripts/master/anti_paywall.js
+// @updateURL    https://raw.githubusercontent.com/abdusco/userscripts/master/anti_paywall.user.js
+// @downloadURL  https://raw.githubusercontent.com/abdusco/userscripts/master/anti_paywall.user.js
 // @grant        none
 // ==/UserScript==
 
