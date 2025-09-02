b19d25211aed0c4fd76e0fe2bebba7db8e1f6aef
Abdussamet Koçak <sssmmt@gmail.com>
2025-09-02 07:49:38 +0200

    chore: Bump version

--- anti_paywall.user.js
+++ anti_paywall.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         anti paywall
 // @namespace    http://tampermonkey.net/
-// @version      0.5
+// @version      0.6
 // @description  try to take over the world!
 // @author       https://github.com/abdusco
 // @match        *://*/*
